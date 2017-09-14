356901a51b472f283ddd1013b492a8619f3fdaaa
houzhiliang <bughou@gmail.com>
2017-09-14 15:53:42 +0800

    update version

--- config/get.go
+++ config/get.go
@@ -32,7 +32,7 @@ func init() {
 
 func usage() {
 	fmt.Fprintf(os.Stderr,
-		`logc watch files, collect content, and push to logd server. (version: 17.9.7)
+		`logc watch files, collect content, and push to logd server. (version: 17.9.14)
 usage: %s <yaml-config-file>
 `, os.Args[0])
 	flag.PrintDefaults()

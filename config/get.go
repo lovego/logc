bcdf8d34b68a28be6070ab0bd8b825514f97dcf8
廖恒晨 <liaohengchen@gmail.com>
2017-10-26 20:05:23 +0800

    fix map[interface{}]interface{} type when unmarshal

--- config/get.go
+++ config/get.go
@@ -47,6 +47,11 @@ func parse(configFile string) Config {
 	if err := yaml.Unmarshal(content, &envConfs); err != nil {
 		log.Fatal(err)
 	}
+	for _, envConf := range envConfs {
+		for _, file := range envConf.Files {
+			cleanFileMapping(file.Mapping)
+		}
+	}
 	env := os.Getenv(`GOENV`)
 	if env == `` {
 		env = `dev`
@@ -61,4 +66,23 @@ func parse(configFile string) Config {
 func notExist(p string) bool {
 	_, err := os.Stat(p)
 	return err != nil && os.IsNotExist(err)
+}
+
+func cleanFileMapping(mapping map[string]map[string]interface{}) {
+	for _, value := range mapping {
+		for k, v := range value {
+			value[k] = cleanupMapValue(v)
+		}
+	}
+}
+
+func cleanupMapValue(value interface{}) interface{} {
+	if data, ok := value.(map[interface{}]interface{}); ok {
+		res := make(map[string]interface{})
+		for k, v := range data {
+			res[fmt.Sprintf("%v", k)] = cleanupMapValue(v)
+		}
+		return res
+	}
+	return value
 }
